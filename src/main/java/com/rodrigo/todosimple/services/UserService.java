41a72032b177e107a96eb6c4669cd8fa735ecf97
Rodrigo <rodrigobaalves.2004@gmail.com>
2024-12-31 16:27:56 -0300

    add exception handler and custom exception

--- src/main/java/com/rodrigo/todosimple/services/UserService.java
+++ src/main/java/com/rodrigo/todosimple/services/UserService.java
@@ -3,6 +3,8 @@ package com.rodrigo.todosimple.services;
 import com.rodrigo.todosimple.models.User;
 import com.rodrigo.todosimple.repositories.TaskRepository;
 import com.rodrigo.todosimple.repositories.UserRepository;
+import com.rodrigo.todosimple.services.exceptions.DataBindingViolationException;
+import com.rodrigo.todosimple.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
-        return user.orElseThrow(() -> new RuntimeException(
+        return user.orElseThrow(() -> new ObjectNotFoundException(
                 "Usuário não encontrado: Id: " + id + ", Tipo: " + User.class.getName()
         ));
     }
@@ -47,7 +49,7 @@ public class UserService {
         try {
             this.userRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Não é possivel excluir pois há entidades relacionadas");
+            throw new DataBindingViolationException("Não é possivel excluir pois há entidades relacionadas");
 
         }
     }
